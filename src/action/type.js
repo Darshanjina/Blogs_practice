4143315f489b2f656dc84b13c69ad503f016e121
Darshanjina <darshanjina1111998@gmail.com>
2020-08-20 11:32:23 +0530

    initial commit

--- src/action/type.js
+++ src/action/type.js
@@ -4,4 +4,4 @@ export const UPDATE_POST = "UPDATE_POST";
 export const DELETE_POST = "DELETE_POST";
 export const GET_SORTED_POST = "GET_SORTED_POST";
 
-export const APIURL = "https://blogs-practice-server.herokuapp.com/";
+export const APIURL = "https://blogs-practice-server.herokuapp.com/api";
